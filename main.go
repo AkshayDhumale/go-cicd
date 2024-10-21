1c862da3e2fe96a1062edb687e066296370efb54
AkshayDhumale <114733103+AkshayDhumale@users.noreply.github.com>
2024-10-21 18:07:43 +0530

    added the code

---
+++ main.go
@@ -0,0 +1,42 @@
+// package main
+
+// import "fmt"
+
+//	func main() {
+//	    fmt.Println("Hello, World!")
+//	}
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"rsc.io/quote"
+)
+
+func health(w http.ResponseWriter, r *http.Request)  {
+	log.Println("testing the health of server")
+	response := map[string]string{
+		"status":"Ok",
+		"timestamp" : time.Now().String(),
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		fmt.Println(quote.Go())
+        // fmt.Fprintf(w, "use like url?token=value : %s\n", r.URL.Query().Get("token"))
+		
+    })
+	
+	http.HandleFunc("/health", health)
+	log.Printf("web server is started...")
+    http.ListenAndServe(":80", nil)
+}
+// adding this comment to check wether the github runner event triggers or not
