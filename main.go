d3f37ca06f9a0cf6a89fff7089b3e6b210894292
AkshayDhumale <114733103+AkshayDhumale@users.noreply.github.com>
2024-11-06 16:32:23 +0530

    chnaging port

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
     })
 	
 	http.HandleFunc("/health", health)
-	log.Printf("web server is started At 8080...")
+	log.Printf("web server is started At 9090...")
     http.ListenAndServe(":80", nil)
 }
 // adding this comment to check wether the github runner event triggers or not
